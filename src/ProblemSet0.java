77c888c15b1a1c63c92d5bb8a5b983ca07b658e2
Nicole Giron <ngiron@ucvts.tec.nj.us>
2019-09-12 08:12:52 -0400

    fifth commit

--- src/ProblemSet0.java
+++ src/ProblemSet0.java
@@ -62,23 +62,24 @@ public class ProblemSet0 {
     System.out.println("  +-----+\n");
 
 
-        /*
-         * Exercise 5.
-         *
-         * Print a to-do list to the console.
-         */
+    /*
+    * Exercise 5.
+    *
+    * Print a to-do list to the console.
+    */
 
-        System.out.println("Here's today's to-do list:");
-        System.out.println("\t- Go food shopping");
-        System.out.println("\t- Mow the lawn\n");
+   System.out.println("Here's today's to-do list:");
+   System.out.println("\t- Go food shopping");
+   System.out.println("\t- Mow the lawn\n");
 
-        /*
-         * Exercise 6.
-         *
-         * Print a square of text to the console.
-         */
+   /*
+    * Exercise 6.
+    *
+    * Print a square of text to the console.
+    */
 
-        System.out.println("H E L L O\nEA\nLP\nLC\nO A P C);
+   System.out.println("H E L L O\nE       A\nL       P\nL       C\nO A P C S");
+
 
         /*
          * Exercise 7.
